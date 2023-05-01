0b8c50a9d2c96f52cc4342aa60f5d6f26aa7b40d
Ivan Dobrokhvalov <dobrokhvalov.official@ya.ru>
2023-05-01 17:43:20 +0300

    RecyclerView and xml files added

--- app/src/main/java/com/example/roomapplicationjava/MainActivity.java
+++ app/src/main/java/com/example/roomapplicationjava/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.roomapplicationjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import android.os.Bundle;
 
@@ -10,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final WordListAdapter adapter = new WordListAdapter(new WordListAdapter.WordDiff());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
